752fecdd0c4301eff36420deb0db202284aa8bd3
staticWagomU <to6wa17@gmail.com>
2024-02-23 22:52:13 +0900

    little update test

--- parseLua_test.ts
+++ parseLua_test.ts
@@ -1,9 +1,19 @@
 import { assertEquals } from "https://deno.land/std@0.217.0/testing/asserts.ts";
 import { parseLua } from "./parseLua.ts";
 
-Deno.test("parseLua", () => {
+Deno.test("空のとき", () => {
   const hooksFile: string[] = [];
   const options = parseLua(hooksFile, "{{{,}}}");
   assertEquals(options, []);
 });
+
+Deno.test("関係ないコメントかコメント行のとき", () => {
+  const hooksFile: string[] = [
+    "-- hoge",
+    "fuga = 1",
+  ];
+  const options = parseLua(hooksFile, "{{{,}}}");
+  assertEquals(options, []);
+});
+
 
