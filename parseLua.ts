183ef28796ea042aa4143b4d8676d1607b368b61
staticWagomU <to6wa17@gmail.com>
2024-02-24 22:48:25 +0900

    repoを取得できるようにした

--- parseLua.ts
+++ parseLua.ts
@@ -1,16 +1,31 @@
-import {
-  Plugin,
-} from "https://deno.land/x/dpp_vim@v0.0.7/types.ts";
+import { Plugin } from "https://deno.land/x/dpp_vim@v0.0.7/types.ts";
 
 export function parseLua(filelines: string[], marker: string): Plugin[] {
   const startMarker = marker.split(",")[0];
   const endMarker = marker.split(",")[1];
+  const luaComment = "--";
+  const plugins: Plugin[] = [];
   for (const line of filelines) {
-    if (line.includes(marker)) {
-      const pattern = new RegExp(`^\\s*--\\s*${startMarker}\\s*repo\\s*:\\s*'(.+)\\s*'${endMarker}\\s*$`);
+    if (
+      line.startsWith(luaComment) && line.includes(startMarker) &&
+      line.includes(endMarker)
+    ) {
+      const endMarkerPos = line.lastIndexOf(endMarker);
+      if (endMarkerPos < 0) continue;
+
+      const match = line.slice(0, endMarkerPos).match(
+        new RegExp(
+          `^${luaComment}\\s*${startMarker}\\s*repo\\s*:\\s*'(?<repoName>.+)'\\s*`,
+        ),
+      );
+
+      if (!match || !match.groups) continue;
+      const repo = match[1];
+
+      if (!repo) continue;
+      plugins.push({ name: repo, repo: repo });
     }
   }
-
-  return [];
+  return plugins;
 }
 
--- parseLua_test.ts
+++ parseLua_test.ts
@@ -1,5 +1,6 @@
 import { assertEquals } from "https://deno.land/std@0.217.0/assert/mod.ts";
 import { parseLua } from "./parseLua.ts";
+import { type Plugin } from "https://deno.land/x/dpp_vim@v0.0.7/types.ts";
 
 Deno.test("空のとき", () => {
   const hooksFile: string[] = [];
@@ -16,4 +17,16 @@ Deno.test("関係ないコメントかコメント行のとき", () => {
   assertEquals(plugins, []);
 });
 
-
+Deno.test("単一のレポジトリだけ設定", () => {
+  const hooksFile: string[] = [
+    "-- {{{ repo: 'hoge' }}}",
+  ];
+  const plugins = parseLua(hooksFile, "{{{,}}}");
+  const expected: Plugin[] = [
+    {
+      name: "hoge",
+      repo: "hoge",
+    },
+  ];
+  assertEquals(plugins, expected);
+});
